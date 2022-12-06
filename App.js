601df4c61dd0efe7f43c8cd64c59e4d81765c07e
Kimberly <kimberly.d102102@gmail.com>
2022-12-06 15:28:43 -0500

    kim w

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>is it working?</Text> 
+      <Text>Text</Text>
       <StatusBar style="auto" />
     </View>
   );
