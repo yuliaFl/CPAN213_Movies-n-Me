50cc98f7e6342cd9252c724f2d4eee1c214dd191
Kimberly <kimberly.d102102@gmail.com>
2022-12-05 21:31:10 -0500

    kim change

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello kim</Text>
+      <Text>hello yulia</Text> 
       <StatusBar style="auto" />
     </View>
   );
