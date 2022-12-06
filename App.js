79332160d829e80a6d6a8c988d295bcf1dc4e2bd
Your Name <you@example.com>
2022-12-06 16:01:02 -0500

    Darien

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Hi Justin</Text> 
       <Text>Hi! -Justin</Text> 
+      <Text>Hi! -DARIEN</Text>
       <StatusBar style="auto" />
     </View>
   );
