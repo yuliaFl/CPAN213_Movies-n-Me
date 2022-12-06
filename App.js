87b1804703ae0ccb55079477bbad6ab26730c97e
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2022-12-06 16:34:31 -0500

    imports

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import homePage from "./components/homePage";
+import login from "./components/login";
+import moviePage from "/components/moviePage";
+import navigation from "/components/navigation";
+import register from "/components/return";
+import watchList from "/components/watchList";
+import welcome from "/components/welcome";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hi Justin</Text> 
-      <Text>Hi! -Justin</Text> 
-      <Text>Hi! -DARIEN</Text>
-      <Text>Hi its Yulia</Text>
       <StatusBar style="auto" />
     </View>
   );
