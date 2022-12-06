8fd8292474d5d6be0dde0ad33342b7a85ea8bdae
Kimberly <kimberly.d102102@gmail.com>
2022-12-06 15:36:53 -0500

    New change

--- App.js
+++ App.js
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>is it working?</Text> 
-      <Text>Text</Text>
+      <Text>plz work</Text>
       <StatusBar style="auto" />
     </View>
   );
