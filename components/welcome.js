7091cf1551529980f5355d51c1a424c7b8a2db81
Kimberly <kimberly.d102102@gmail.com>
2022-12-07 12:03:18 -0500

    welcome page -kim

--- components/welcome.js
+++ components/welcome.js
@@ -1,27 +1,49 @@
-import * as React from 'react'
+import * as React from "react";
 import {
-    StyleSheet,
-    Text,
-    View,
-    Button,
-    FlatList,
-    TextInput,
-    NativeModules,
-  } from "react-native";
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  FlatList,
+  TextInput,
+  NativeModules,
+} from "react-native";
+import logo_proto from "./logo/groupFinal_logo_proto.png";
 
-  export default function App() {
+export default function App() {
+  const logoFadeIn = (props) => {
+    const n = useRef(new Animated.Value(0)).current;
+
+    useEffect(() => {
+      Animated.timing(n, {
+        toValue: 1,
+        duration: 4000,
+      }).start();
+    }, [n]);
+
     return (
-<h1>MOVIES AND ME</h1>
-    )
-  }
+      <Animated.View style={{ opacity: n }}>{props.children}</Animated.View>
+    );
+  };
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection:"column",
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-      paddingHorizontal: 10
-    },
-  });
+  return (
+    <View style={styles.Screen}>
+      <View>
+        <logoFadeIn>
+          <img src={logo_proto} alt="Logo"/>
+        </logoFadeIn>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 10,
+  },
+});
