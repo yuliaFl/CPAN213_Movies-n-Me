c2f02bed60d993044802f85581e9810946852f66
Kimberly <kimberly.d102102@gmail.com>
2022-12-07 12:34:36 -0500

    welcome page + progress file -kim

--- components/welcome.js
+++ components/welcome.js
@@ -9,6 +9,7 @@ import {
   NativeModules,
 } from "react-native";
 import logo_proto from "./logo/groupFinal_logo_proto.png";
+import ProgressBar from "./components/welcomeProgress";
 
 export default function App() {
   const logoFadeIn = (props) => {
@@ -30,9 +31,17 @@ export default function App() {
     <View style={styles.Screen}>
       <View>
         <logoFadeIn>
-          <img src={logo_proto} alt="Logo"/>
+          <img src={logo_proto} alt="Logo" />
         </logoFadeIn>
       </View>
+      <ProgressBar
+        progress={duration}
+        max={100}
+        min={0}
+        barColor={bar}
+        borderColor={"white"}
+      />
+      <View></View>
     </View>
   );
 }
@@ -41,7 +50,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
-    backgroundColor: "#fff",
+    backgroundColor: "navy",
     alignItems: "center",
     justifyContent: "center",
     paddingHorizontal: 10,
