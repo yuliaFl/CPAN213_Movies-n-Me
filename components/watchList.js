aa6cbba2c7632f11a7da09b770f8c0200690697c
JustinKP <jkpcraft@hotmail.com>
2022-12-07 16:19:22 -0500

    got rid of vestigial products thing from redux

--- components/watchList.js
+++ components/watchList.js
@@ -7,26 +7,12 @@ import {
   removeFromCart,
 } from '../redux/actions/index';
 
+
 const WatchListPage = ({
-  products,
   cartProducts,
   addProductToCart,
   removeProductFromCart,
-  fetchProductsForComponent,
-}) => {
-  React.useEffect(() => {
-    fetchProductsForComponent();
-  }, []);
-
-  const renderProduct = ({ item }) => (
-    <View>
-      <Text>{item.title}</Text>
-      <Text>$ {item.price}</Text>
-      <Text>{item.description}</Text>
-      <Button title="Add to WatchList" onPress={() => addProductToCart(item)} />
-      <View style={{ borderBottomColor: 'black', borderWidth: 1, margin: 5 }} />
-    </View>
-  );
+}) => {  
   const renderCartProduct = ({ item }) => (
     <View>
       <Text>{item.title}</Text>
@@ -39,7 +25,6 @@ const WatchListPage = ({
 
   return (
     <View>
-      <FlatList data={products} renderItem={renderProduct} />
       <View style={{ margin: 5 }} />
       <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Your WatchList:</Text>
       <FlatList data={cartProducts} renderItem={renderCartProduct} />
@@ -50,7 +35,6 @@ const WatchListPage = ({
 // Needed to convert redux state to props for the component
 const mapStateToProps = (state) => {
   return {
-    products: state.productsReducer.products,
     cartProducts: state.cartReducer.products,
   };
 };
@@ -58,7 +42,6 @@ const mapStateToProps = (state) => {
 // Needed to convert redux actions to props for the component
 const mapDispatchToProps = (dispatch) => {
   return {
-    fetchProductsForComponent: () => fetchProducts(dispatch),
     addProductToCart: (product) => dispatch(addToCart(product)),
     removeProductFromCart: (product) => dispatch(removeFromCart(product)),
   };
--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,13 +1,10 @@
 import { combineReducers } from 'redux';
-import productsReducer from './products';
 import cartReducer from './cart'
 
 // const state = {
-//   productsReducer: {
-//    products: [] 
+//     cartReducer: {
+//        products: []
+//     }
 // }
-// cartReducer: {
-//    products: []
-// }
-// }
-export default combineReducers({ productsReducer, cartReducer })
+
+export default combineReducers({ cartReducer })
