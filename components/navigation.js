6304a7c13956d9e8f53f234555cf3690ed0b47ef
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2022-12-08 12:00:52 -0500

    nav change

--- components/navigation.js
+++ components/navigation.js
@@ -8,24 +8,14 @@ import {
   TextInput,
   NativeModules,
 } from "react-native";
-import Register from "./register";
-import Login from "./login";
-import HomePage from "./homePage";
-import { createDrawerNavigator } from '@react-navigation/drawer';
 
-const Drawer = createDrawerNavigator();
+export default function Navigation ({navigation}){
+  return(
+    <h1>nav</h1>
 
-function Navigation() {
-  return (
-      <Drawer.Navigator initialRouteName="Home">
-          <Drawer.Screen name="Home" component={Screen1} />
-          <Drawer.Screen name="Settings" component={Screen2} />
-          <Drawer.Screen name="Contacts" component={Screen3} />
-      </Drawer.Navigator>
-  ); 
+  )
 }
 
-export default Navigation;
 
 
 const styles = StyleSheet.create({
