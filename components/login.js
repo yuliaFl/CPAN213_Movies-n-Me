b8ec917c4a7d20cdf9e22d0b5c0425a60228bd21
Your Name <you@example.com>
2022-12-10 19:39:03 -0500

    Login_modified

--- components/login.js
+++ components/login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   View,
   StyleSheet,
@@ -7,8 +7,8 @@ import {
   Button,
   TouchableOpacity,
   Modal,
-} from "react-native";
-import { Checkbox, NativeBaseProvider } from "native-base";
+} from 'react-native';
+import { Checkbox, NativeBaseProvider } from 'native-base';
 
 function Seperator() {
   return <View style={styles.seperator}></View>;
@@ -16,23 +16,37 @@ function Seperator() {
 
 function LogingForm() {
   const [showModal, setShowModal] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
   const [state, setState] = useState({
-    email: "",
-    error: "",
-    password: "",
+    email: '',
+    emailError: '',
+    passwordError: '',
+    checkBoxErr: '',
+    password: '',
   });
+
   const inputValidator = () => {
-    if (state.email == "") {
-      setState({ ...state, error: "Email field Cannot be empty" });
+    if (state.email == '') {
+      setState({ emailError: 'Email Cannot be empty' });
     } else {
-      setState({ ...state, error: "" });
+      setState({ emailError: '' });
     }
   };
+  const validPassoword = () => {
+    if (state.password > 9) {
+      setState({ passwordError: 'Password must be 8 character max' });
+    } else if (state.password == '') {
+      setState({ passwordError: 'Password cannot be empty' });
+    } else {
+      setState({ passwordError: '' });
+    }
+  };
+
   return (
     <NativeBaseProvider>
       <View style={styles.Screen}>
         <Text style={styles.title}>Login Form</Text>
-        <Text style={styles.error}>{state.error}</Text>
+        <Text style={styles.error}>{state.emailError}</Text>
         <TextInput
           style={styles.input_container}
           placeholder="Email Address"
@@ -44,7 +58,7 @@ function LogingForm() {
           marginX={10}
           marginY={5}
         />
-        <Text style={styles.error}>{state.error1}</Text>
+        <Text style={styles.error}>{state.passwordError}</Text>
         <TextInput
           style={styles.input_container}
           placeholder="Password"
@@ -52,6 +66,7 @@ function LogingForm() {
           onChangeText={(text) => {
             setState({ password: text });
           }}
+          onBlur={() => validPassoword()}
           variant="rounded"
           marginX={10}
         />
@@ -61,8 +76,7 @@ function LogingForm() {
           visible={showModal}
           onRequestClose={() => {
             setShowModal(!showModal);
-          }}
-        >
+          }}>
           <View style={styles.modalContainer}>
             <View style={styles.modalView}>
               <Text style={styles.modalText}>
@@ -71,17 +85,13 @@ function LogingForm() {
               <View style={styles.modalBtnRow}>
                 <TouchableOpacity
                   style={styles.modalButton}
-                  onPress={() => navigation.navigate("Home")}
-                >
+                  onPress={() => navigation.navigate('Home')}>
                   <Text style={styles.modalBtnText}> YES </Text>
                 </TouchableOpacity>
-                <br /><Seperator />
-                <Seperator />
 
                 <TouchableOpacity
                   style={styles.modalButton}
-                  onPress={() => setShowModal(!showModal)}
-                >
+                  onPress={() => setShowModal(!showModal)}>
                   <Text style={styles.modalBtnText}> NO </Text>
                 </TouchableOpacity>
               </View>
@@ -91,8 +101,7 @@ function LogingForm() {
 
         <TouchableOpacity
           style={styles.button}
-          onPress={() => setShowModal(!showModal)}
-        >
+          onPress={() => setShowModal(!showModal)}>
           <Text style={styles.buttonText}> Login </Text>
         </TouchableOpacity>
         <Seperator />
@@ -100,7 +109,8 @@ function LogingForm() {
         <Seperator />
 
         <View>
-          <Checkbox>
+          <Text style={styles.error}>{state.checkBoxErr}</Text>
+          <Checkbox value={isSelected} onValueChange={setIsSelected}>
             <Text fontSize="md" style={styles.checkBox}>
               Remember Password?
             </Text>
@@ -116,69 +126,69 @@ export default LogingForm;
 const styles = StyleSheet.create({
   Screen: {
     flex: 1,
-    flexDirection: "column",
-    backgroundColor: "#2B2882",
-    alignItems: "center",
-    justifyContent: "center",
+    flexDirection: 'column',
+    backgroundColor: '#2B2882',
+    alignItems: 'center',
+    justifyContent: 'center',
     paddingHorizontal: 10,
   },
   title: {
     fontSize: 36,
-    fontWeight: "bold",
-    color: "#F2E5CE",
+    fontWeight: 'bold',
+    color: '#F2E5CE',
     marginBottom: 25,
   },
   modalContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 22,
   },
   input_container: {
-    backgroundColor: "#000000",
+    backgroundColor: '#000000',
     borderRadius: 12,
     margin: 10,
     shadowRadius: 2,
     padding: 14,
-    color: "#F9BC08",
-    width: "80%",
+    color: '#F9BC08',
+    width: '80%',
   },
 
   error: {
-    color: "red",
+    color: 'red',
   },
   button: {
-    backgroundColor: "#E63169",
-    display: "flex",
+    backgroundColor: '#E63169',
+    display: 'flex',
     height: 60,
     borderRadius: 100,
     marginTop: 35,
-    justifyContent: "center",
-    alignItems: "center",
-    width: "75%",
-    shadowColor: "hotpink",
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '75%',
+    shadowColor: 'hotpink',
     shadowOpacity: 0.9,
     shadowRadius: 10,
   },
   buttonText: {
     fontSize: 17,
-    fontWeight: "bold",
-    color: "white",
+    fontWeight: 'bold',
+    color: 'white',
   },
   checkBox: {
-    color: "#F9BC08",
+    color: '#F9BC08',
     fontSize: 14,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   modalButton: {
-    backgroundColor: "#F9BC08",
+    backgroundColor: '#F9BC08',
     width: 70,
     height: 50,
     borderRadius: 20,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
 
-    shadowColor: "#F2E5CE",
+    shadowColor: '#F2E5CE',
     shadowOpacity: 0.9,
     shadowRadius: 10,
   },
@@ -189,24 +199,24 @@ const styles = StyleSheet.create({
   },
   modalBtnText: {
     fontSize: 16,
-    fontWeight: "bold",
-    color: "#2B2882",
+    fontWeight: 'bold',
+    color: '#2B2882',
   },
   modalText: {
     margin: 15,
-    fontWeight: "bold",
-    textAlign: "center",
-    color: "#E63169",
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#E63169',
   },
   modalView: {
-    width: "80%",
+    width: '80%',
     height: 180,
     borderWidth: 2,
     borderRadius: 15,
-    borderColor: "#E63169",
+    borderColor: '#E63169',
     padding: 15,
-    alignItems: "center",
-    backgroundColor: "#000000",
+    alignItems: 'center',
+    backgroundColor: '#000000',
   },
   seperator: {
     margin: 6,
